5c19399472c36af1e775a1327b3ccc3635102beb
Simon Milord <simon.milord@gmail.com>
2025-04-22 16:18:53 -0400

    refactored the validation of metrics in its own utility function

--- src/components/companyMetricsCard/companyMetricsCard.tsx
+++ src/components/companyMetricsCard/companyMetricsCard.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import {roundToDecimal} from '@utils/functions';
 import { Paper, Title, Divider, Table, Box } from "@mantine/core";
-import { formatDollarAmount } from "@utils/functions";
+import { formatDollarAmount, validateMetricsValue, roundToDecimal } from "@utils/functions";
 import "../cardStyles.css";
 
 type RowItem = {
   key: String;
   value: String;
-}
+};
 
 export default function CompanyMetricsCard({
   metricsData,
@@ -21,87 +20,191 @@ export default function CompanyMetricsCard({
   reportedFinancialData: any;
 }) {
   const notAvailable: string = "N/A";
-  const metrics = metricsData?.metric;
-  const fcfPerShare = metricsData?.series?.quarterly?.fcfPerShareTTM[0].v;
-  const stockPrice = quoteData?.c;
+  const metrics: any = metricsData?.metric;
+  const fcfPerShare: number =
+    metricsData?.series?.quarterly?.fcfPerShareTTM[0].v;
+  const stockPrice: number = quoteData?.c;
 
   const getFCFYield = () => {
-    const fcfYield = fcfPerShare / stockPrice * 100;
-    return `${roundToDecimal(fcfYield, 2)}` || notAvailable;
-  }
+    const fcfYield = (fcfPerShare / stockPrice) * 100;
+    return `${roundToDecimal(fcfYield, 2)}%` || notAvailable;
+  };
 
   const getPriceToFCF = () => {
     return `${roundToDecimal(stockPrice / fcfPerShare, 2)}` || notAvailable;
-  }
+  };
 
   const getFCFperShareGrowth = (period: number): string => {
     const fcfPerShareArray = metricsData?.series?.quarterly?.fcfPerShareTTM;
     const previousIndex: number = 4 * period;
 
     const latestFCFperShValue = fcfPerShareArray && fcfPerShareArray[0]?.v;
-    const previousFCFperShValue = fcfPerShareArray && fcfPerShareArray[previousIndex]?.v;
+    const previousFCFperShValue =
+      fcfPerShareArray && fcfPerShareArray[previousIndex]?.v;
 
     if (latestFCFperShValue > 0 && previousFCFperShValue > 0) {
-      const fcfCagr = ((latestFCFperShValue / previousFCFperShValue) ** (1 / period) - 1) * 100;
+      const fcfCagr =
+        ((latestFCFperShValue / previousFCFperShValue) ** (1 / period) - 1) *
+        100;
       return `${roundToDecimal(fcfCagr, 2)}%`;
     } else {
-      return '--.--%';
+      return "--.--%";
     }
-  }
+  };
 
   const retrieveCashOnBalanceSheet = (): number => {
     const sharesOutstanding = profileData?.shareOutstanding;
     const cashPerShare = metrics?.cashPerSharePerShareAnnual;
 
     return roundToDecimal(cashPerShare * sharesOutstanding, 2) * 1000000;
-  }
+  };
 
   const retrieveDebtOnBalanceSheet = (): number => {
-    const longTermDebtConceptTarget = 'us-gaap_LongTermDebtNoncurrent';
-    const longTermDebtTargetLabel = 'Long-term debt';
+    const longTermDebtConceptTarget = "us-gaap_LongTermDebtNoncurrent";
+    const longTermDebtTargetLabel = "Long-term debt";
 
     const balanceSheetEntries = reportedFinancialData?.report?.bs;
-    const longTermDebt = balanceSheetEntries?.find((entry: any) => entry?.concept === longTermDebtConceptTarget || entry?.label === longTermDebtTargetLabel);
+    const longTermDebt = balanceSheetEntries?.find(
+      (entry: any) =>
+        entry?.concept === longTermDebtConceptTarget ||
+        entry?.label === longTermDebtTargetLabel
+    );
 
     return roundToDecimal(longTermDebt?.value, 2);
-  }
+  };
 
   const companyValuationItems = [
-    { key: "PE (TTM):", value: `${roundToDecimal(metrics?.peTTM, 2)}` || notAvailable },
-    { key: "P/S:", value: `${roundToDecimal(metrics?.psTTM, 2)}` || notAvailable },
-    { key: "P/B:", value: `${roundToDecimal(metrics?.pbAnnual, 2)}` || notAvailable },
-    { key: "P/FCF:", value: `${getPriceToFCF()}` || notAvailable },
-    { key: `FCF Yield ($${roundToDecimal(fcfPerShare, 2)} / $${roundToDecimal(stockPrice, 2)}):`, value: `${getFCFYield()}%` || notAvailable },
+    {
+      key: "PE (TTM):",
+      value: validateMetricsValue(`${roundToDecimal(metrics?.peTTM, 2)}`),
+    },
+    {
+      key: "P/S:",
+      value: validateMetricsValue(`${roundToDecimal(metrics?.psTTM, 2)}`),
+    },
+    {
+      key: "P/B:",
+      value: validateMetricsValue(`${roundToDecimal(metrics?.pbAnnual, 2)}`),
+    },
+    { key: "P/FCF:", value: `${getPriceToFCF()}` },
+    {
+      key: `FCF Yield ($${roundToDecimal(fcfPerShare, 2)} / $${roundToDecimal(
+        stockPrice,
+        2
+      )}):`,
+      value: `${getFCFYield()}`,
+    },
   ];
 
   const companyGrowthItems = [
-    { key: "Rev growth TTM:", value: `${roundToDecimal(metrics?.revenueGrowthTTMYoy, 2)}%` || notAvailable },
-    { key: "Rev growth 3Y:", value: `${roundToDecimal(metrics?.revenueGrowth3Y, 2)}%` || notAvailable },
-    { key: "Rev growth 5Y:", value: `${roundToDecimal(metrics?.revenueGrowth5Y, 2)}%` || notAvailable },
-    { key: "EPS growth TTM:", value: `${roundToDecimal(metrics?.epsGrowthTTMYoy, 2)}%` || notAvailable },
-    { key: "EPS growth 3Y:", value: `${roundToDecimal(metrics?.epsGrowth3Y, 2)}%` || notAvailable },
-    { key: "EPS growth 5Y:", value: `${roundToDecimal(metrics?.epsGrowth5Y, 2)}%` || notAvailable },
+    {
+      key: "Rev growth TTM:",
+      value: validateMetricsValue(metrics?.revenueGrowthTTMYoy, "%"),
+    },
+    {
+      key: "Rev growth 3Y:",
+      value: validateMetricsValue(metrics?.revenueGrowth3Y, "%"),
+    },
+    {
+      key: "Rev growth 5Y:",
+      value: validateMetricsValue(metrics?.revenueGrowth5Y, "%"),
+    },
+    {
+      key: "EPS growth TTM:",
+      value: validateMetricsValue(metrics?.epsGrowthTTMYoy, "%"),
+    },
+    {
+      key: "EPS growth 3Y:",
+      value: validateMetricsValue(metrics?.epsGrowth3Y, "%"),
+    },
+    {
+      key: "EPS growth 5Y:",
+      value: validateMetricsValue(metrics?.epsGrowth5Y, "%"),
+    },
     { key: "FCF/Sh growth 3Y:", value: `${getFCFperShareGrowth(3)}` },
     { key: "FCF/Sh growth 5Y:", value: `${getFCFperShareGrowth(5)}` },
   ];
 
   const companyProfitabilityItems = [
-    { key: "Gross margin TTM:", value: `${roundToDecimal(metrics?.grossMarginTTM, 2)}%` || notAvailable },
-    { key: "Operating margin TTM:", value: `${roundToDecimal(metrics?.operatingMarginTTM, 2)}%` || notAvailable },
-    { key: "Profit margin TTM:", value: `${roundToDecimal(metrics?.netProfitMarginTTM, 2)}%` || notAvailable },
-    { key: "ROA:", value: `${roundToDecimal(metrics?.roaTTM, 2)}%` || notAvailable },
-    { key: "ROE:", value: `${roundToDecimal(metrics?.roeTTM, 2)}%` || notAvailable },
-    { key: "ROIC:", value: `${roundToDecimal(metricsData?.series?.annual?.roic[0].v * 100, 2)}%` || notAvailable },
-    { key: "FCF margin:", value: `${roundToDecimal(metricsData?.series?.annual?.fcfMargin[0].v * 100, 2)}%` || notAvailable },
+    {
+      key: "Gross margin TTM:",
+      value: validateMetricsValue(metrics?.grossMarginTTM, "%"),
+    },
+    {
+      key: "Operating margin TTM:",
+      value: validateMetricsValue(metrics?.operatingMarginTTM, "%"),
+    },
+    {
+      key: "Profit margin TTM:",
+      value: validateMetricsValue(metrics?.netProfitMarginTTM, "%"),
+    },
+    {
+      key: "ROA:",
+      value: validateMetricsValue(metrics?.roaTTM, "%"),
+    },
+    {
+      key: "ROE:",
+      value: validateMetricsValue(metrics?.roeTTM, "%"),
+    },
+    {
+      key: "ROIC:",
+      value: validateMetricsValue(
+        `${roundToDecimal(metricsData?.series?.annual?.roic[0].v * 100, 2)}`,
+        "%"
+      ),
+    },
+    {
+      key: "FCF margin:",
+      value:
+        `${roundToDecimal(
+          metricsData?.series?.annual?.fcfMargin[0].v * 100,
+          2
+        )}%` || notAvailable,
+    },
   ];
 
   const companyFinHealthItems = [
-    { key: "Cash:", value: `${formatDollarAmount(retrieveCashOnBalanceSheet())}` || notAvailable },
-    { key: "LT Debt:", value: `${formatDollarAmount(retrieveDebtOnBalanceSheet())}` || notAvailable }, // TODO: find a reliable source for this
-    { key: "Net Cash position:", value: `${formatDollarAmount((retrieveCashOnBalanceSheet() - retrieveDebtOnBalanceSheet()))}` || notAvailable },
-    { key: "Div/sh:", value: `${metrics?.dividendPerShareTTM === null ? notAvailable : roundToDecimal(metrics?.dividendPerShareTTM, 2)}$`},
-    { key: "Div yield:", value: `${metrics?.currentDividendYieldTTM === null ? notAvailable : metrics?.currentDividendYieldTTM}%`},
-    { key: "Payout ratio:", value: `${metrics?.payoutRatioTTM === null ? notAvailable : metrics?.payoutRatioTTM}%`},
+    {
+      key: "Cash:",
+      value: validateMetricsValue(
+        formatDollarAmount(retrieveCashOnBalanceSheet())
+      ),
+    },
+    {
+      key: "LT Debt:",
+      value: validateMetricsValue(
+        formatDollarAmount(retrieveDebtOnBalanceSheet())
+      ),
+    },
+    {
+      key: "Net Cash position:",
+      value: validateMetricsValue(
+        formatDollarAmount(
+          retrieveCashOnBalanceSheet() - retrieveDebtOnBalanceSheet()
+        )
+      ),
+    },
+    {
+      key: "Div/sh:",
+      value: validateMetricsValue(
+        `${roundToDecimal(metrics?.dividendPerShareTTM, 2)}`,
+        "$"
+      ),
+    },
+    {
+      key: "Div yield:",
+      value: validateMetricsValue(
+        `${roundToDecimal(metrics?.currentDividendYieldTTM, 2)}`,
+        "%"
+      ),
+    },
+    {
+      key: "Payout ratio:",
+      value: validateMetricsValue(
+        `${roundToDecimal(metrics?.payoutRatioTTM, 2)}`,
+        "%"
+      ),
+    },
   ];
 
   const buildRows = (items: RowItem[]) => {
@@ -111,7 +214,7 @@ export default function CompanyMetricsCard({
         <Table.Td className="tableRow tableRow__value">{item.value}</Table.Td>
       </Table.Tr>
     ));
-  }
+  };
 
   return (
     <div>
@@ -127,18 +230,27 @@ export default function CompanyMetricsCard({
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>{buildRows(companyValuationItems)}</Table.Tbody>
+        </Table>
+        <Box mb="lg" />
+        <Table withRowBorders={false}>
           <Table.Thead>
             <Table.Tr>
               <Table.Th className="tableHead">Growth</Table.Th>
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>{buildRows(companyGrowthItems)}</Table.Tbody>
+        </Table>
+        <Box mb="lg" />
+        <Table withRowBorders={false}>
           <Table.Thead>
             <Table.Tr>
               <Table.Th className="tableHead">Profitability</Table.Th>
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>{buildRows(companyProfitabilityItems)}</Table.Tbody>
+        </Table>
+        <Box mb="lg" />
+        <Table withRowBorders={false}>
           <Table.Thead>
             <Table.Tr>
               <Table.Th className="tableHead">Financial Health</Table.Th>
